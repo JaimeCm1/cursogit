5970d01c2a52319450da334ab853c42cf1e9e02a
Jaime Jovany <ccomodin89@gmail.com>
2023-03-24 14:07:18 -0600

    Se agrego el gitignore

---
+++ Holamundo.java
@@ -0,0 +1,17 @@
+
+package holamundo;
+
+/**
+ *
+ * @author ALUMNO
+ */
+ public class HolaMundo{
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hola miniwin");
+    }
+
+}
