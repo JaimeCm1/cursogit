5cc1ba994d6d026785b8d570e8c3820d44600439
Jaime Jovany <ccomodin89@gmail.com>
2023-03-24 13:59:30 -0600

    Nuevo cambio

---
+++ holamundoo/Primer cambio/src/holamundoo/Holamundoo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoo;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class Holamundoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Holamundooooooooooooooooooooo");
+        System.out.println("hola");
+        System.out.println("primer cambio");
+
+    }
+
+}
